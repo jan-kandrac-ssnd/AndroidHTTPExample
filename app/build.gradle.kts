55c86201dfefadab01111caa02424dd114b705a1
Jan Kandrac <jan@kandrac.xyz>
2025-12-04 08:44:29 +0100

    Added OkHttp and sample request
    Pre pridanie OkHttp do projektu pozri app/build.gradle.kts
    Pre povolenie INTERNET permission pozri app/src/main/AndroidManifest.xml
    Pre implementaciu GET requestu pozri app/src/main/java/sk/ssnd/okhttpgson/MainActivity.kt
    
    Request sa musi volat na vedlajsom vlakne. To zabezpecis pomocou
    val scope = rememberCoroutineScope()
    scope.launch(Dispatchers.IO) { call(...) }

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ android {
 
 dependencies {
 
+  implementation("com.squareup.okhttp3:okhttp:5.3.2")
+
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.androidx.activity.compose)
--- app/src/main/java/sk/ssnd/okhttpgson/MainActivity.kt
+++ app/src/main/java/sk/ssnd/okhttpgson/MainActivity.kt
@@ -4,6 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+// GET -> https://dummyjson.com/users/1
+
+// Builder pattern -> OOP design patterns
+fun call(url: String): String {
+  val client = OkHttpClient()
+  val request = Request.Builder()
+    .url(url)
+    .get()
+    .build()
+  val call = client.newCall(request)
+  val response = call.execute()
+  return response.body.string()
+}
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +37,18 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
 
     setContent {
+      Column(modifier = Modifier.systemBarsPadding()) {
+        val scope = rememberCoroutineScope()
+        val response = remember { mutableStateOf("") }
 
+        Button(onClick = {
+          scope.launch(Dispatchers.IO) {
+            response.value = call("https://dummyjson.com/users/1")
+          }
+        }) { Text("Download") }
+
+        Text(response.value)
+      }
     }
   }
 }
